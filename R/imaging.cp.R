fcbc6240a40310ecee7ce4df5e745d3522cd32a2
JMMaronge <Jmaro2@lsuhsc.edu>
2016-08-07 19:30:38 -0400

    added imaging.cp

--- R/imaging.cp.R
+++ R/imaging.cp.R
@@ -1,4 +1,15 @@
- 
+#' A function to create cross-product matrix for MEDALS
+#'
+#' This function allows you to create the cross-product matrix ($X^TX$) for MEDALS. This is the matrix the PCA will be performed on. This is the 2nd step in the MEDALS pipeline.
+#' @param path.img.list A list of sublists, where each sublist is a list of paths to the files for each imaging modality (ex. T1w, T2w, DWI, etc.) for a particular subject. This has to contain at least one sublist
+#' @param path.mask.list A list of paths to the brain mask for each subject. This should be in the same order as the sublists for path.img.list
+#' @param mean.vec A vector of column means of $X$. This will be used to center columns before the PCA.
+#' @param sd.vec A vector of column standard deviations of $X$. This will be used to scale columns before the PCA.
+#' @keywords MEDALS, PCA, Segmentation
+#' @export
+#' @examples
+#' imaging.suff.stat()
+
 imaging.cp.mat<-function(path.img.list,path.mask.list,mean.vec,sd.vec){
   require(ANTsR)
   require(extrantsr)
