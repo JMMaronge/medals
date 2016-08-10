b7e5534aafee2f16c4f7342aea1bb17d403ddbfd
JMMaronge <Jmaro2@lsuhsc.edu>
2016-08-10 11:41:16 -0400

    Changes to vignette, added 2nd order data

---
+++ R/def.fit.2.R
@@ -0,0 +1,5 @@
+#' Default fit of order 2 for medals
+#'
+#' Default model fit from training data for MEDALS package. This fit used 9 subjects with 4 imaging modalities (FLAIR, T1w,T2w,DWI), and carried out a 2nd order MEDALS analysis using the first 8 PCs as predictors.
+#'
+"def.fit.2"
---
+++ R/def.loads.2.R
@@ -0,0 +1,5 @@
+#' Default loadings for 2nd order medals
+#'
+#' Default loadings matrix from training data for MEDALS package. This fit used 9 subjects with 4 imaging modalities (FLAIR, T1w,T2w,DWI), and carried out a 2nd order MEDALS analysis
+#'
+"def.loads.2"
