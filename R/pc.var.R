399bde838289d6e3e4cd6b7ea49d5385d1095955
muschellij2 <muschellij2@gmail.com>
2016-08-17 17:42:02 -0400

    Edited more and added contributor

--- R/pc.var.R
+++ R/pc.var.R
@@ -2,15 +2,17 @@
 #'
 #' This function returns the variance explained by each PC and the cumulative variance explained in the medals package, this should be used with the output of imaging.cp
 #' @param mat The crossproduct matrix, this can be the output of imaging.cp
-#' @param n The number of observations in the original matrix. This can be the output for imaging.suff.stat 
+#' @param n The number of observations in the original matrix. This can be the output for imaging.suff.stat
 #' @keywords MEDALS, variance
 #' @export
+#' @return List of 2 elements: \code{var}: variance of each
+#' component, \code{cum_pct}: cumulative percent variance explained
 #' @examples
 #' pc.var()
 
-pc.var<-function(mat,n){
-  var.pc<-svd(mat)$d/(n-1)
-  cumvar<-cumsum(var.pc)/sum(var.pc)
-  out<-list(var.pc,cumvar)
+pc.var <- function(mat,n){
+  var.pc <- svd(mat)$d / (n - 1)
+  cumvar <- cumsum(var.pc)/sum(var.pc)
+  out <- list(var = var.pc, cum_pct = cumvar)
   return(out)
 }
