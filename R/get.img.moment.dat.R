847404c7fcfee3dde678ea7d122131b945759a2f
muschellij2 <muschellij2@gmail.com>
2016-08-17 16:58:24 -0400

    fixing some parts of mask in get.img.moment.dat

--- R/get.img.moment.dat.R
+++ R/get.img.moment.dat.R
@@ -1,6 +1,6 @@
 #' A function to calculate the matrix of neighborhood moments for a subject MEDALS
 #'
-#' This function allows you to calculate the matrix of neighborhood moments for MEDALS. 
+#' This function allows you to calculate the matrix of neighborhood moments for MEDALS.
 #' @param imgs.path A list of paths to images for one subject.
 #' @param path.mask.list A path to the braiin mask for each subject.
 #' @param mpower A scalar specifying the highest moment wanted for the MEDALS analysis.
@@ -12,26 +12,27 @@
 #' @examples
 #' get.img.moment.dat()
 
-get.img.moment.dat<-function(imgs.path,mask.path,mpower=4){
-  nmod.power = 27*length(imgs.path)*mpower
-    f.mask<-mask.path
-    mask = antsImageRead(f.mask)
-    n = sum(mask)
-    rm(list="mask"); gc();
-    x_i = matrix(nrow = n, ncol = nmod.power)
-    ind = 0
-    for(j in 1:length(imgs.path)){ 
-      vals<-t(neighborhood(img=imgs.path[[j]],
-                           mask=f.mask,
+get.img.moment.dat <- function(imgs.path,mask.path,mpower=4){
+  nmod.power = 27 * length(imgs.path) * mpower
+  mask = check_ants(mask.path)
+  n = sum(mask)
+  x_i = matrix(nrow = n, ncol = nmod.power)
+  ind = 0
+  for (j in 1:length(imgs.path)) {
+    vals <- t(neighborhood(img = imgs.path[[j]],
+                           mask = mask,
                            radius = rep(1,3),
-                           boundary.condition="mean")$values)
-      for (k in 1:mpower){
-        x_i[, seq(ind + 1, ind + 27)] = vals^k
-        ind = ind + 27
-      }
-      rm(list= "vals");
-      eval(gc(),parent.frame())
+                           boundary.condition = "mean")$values
+    )
+    for (k in 1:mpower) {
+      x_i[, seq(ind + 1, ind + 27)] = vals^k
+      ind = ind + 27
     }
-    return(x_i)
-    
-}   
+    rm(list = "vals");
+    eval(gc(), parent.frame())
+  }
+  rm(list = "mask"); gc();
+  eval(gc(),parent.frame())
+  return(x_i)
+
+}
